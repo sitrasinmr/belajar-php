cb59f983d2dc8bb8488835d16f6fd055c68f8532
Sitrasin M Ramping <sitrasinmr@gmail.com>
2022-10-24 11:04:20 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+   echo "Hello World!<br>";
+
+   $nama = "Sitrasin";
+   $umur = 22;
+
+   echo "Nama saya <strong>$nama</strong>, Saya berusia $umur tahun.<br>";
+
+   $namaAdik = "Putri";
+   $umurAdik = 12;
+
+   echo "Nama adik saya <strong>$namaAdik</strong>, adik saya berusia $umurAdik tahun.<br>";
+
+   $selisihUmur = $umur - $umurAdik;
+   echo "Selisih usia dan adik saya adalah $selisihUmur tahun."
+
+?>
